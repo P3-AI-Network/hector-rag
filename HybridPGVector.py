8ac0270788c3366225d21e96cc976b389dd8e3d8
Swapnil Shinde <swapnilshinde9382@gmail.com>
2025-02-14 02:56:30 +0530

    search with ranking and score | query optimizations

--- HybridPGVector.py
+++ HybridPGVector.py
@@ -2,14 +2,16 @@ import os
 import psycopg2
 import json 
 import logging
+import psycopg2.extras
+
 from uuid import uuid4
 
-from typing import TypedDict, List
+from typing import List, TypedDict, Dict, Optional
+from dataclasses import dataclass
 
 from langchain_core.documents import Document
 from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_core.embeddings import Embeddings
-import psycopg2.extras
 
 class PGConnection(TypedDict):
     user: str
@@ -20,7 +22,7 @@ class PGConnection(TypedDict):
 
 class HybridPGVector:
 
-    def __init__(self, connection:PGConnection, embeddings: Embeddings, collection_name: str, collection_metada: dict) -> None:
+    def __init__(self, connection: PGConnection, embeddings: Embeddings, collection_name: str, collection_metada: dict) -> None:
 
         self.connection = psycopg2.connect(
             user=connection['user'],
@@ -38,78 +40,250 @@ class HybridPGVector:
         self.collection_uuid = None
 
         self.embeddings = embeddings
-        self.embedding_dimension = len(embeddings.embed_query("Get Embedding"))
+        self.embedding_dimension = len(embeddings.embed_query("Get Embedding"))\
+        
+        self.rrf_constant = 60
 
         self._init_tables()
         self._create_collection()
 
-    def _init_tables(self):
+    def similarity_search(self, query: str, document_limit: int) -> List[Document]:
+        
+        doc_ranking = self.similarity_search_with_ranking(query, document_limit)
+        docs = doc_ranking.keys()
+        return self._fetch_documents(list(docs))
+    
+    def similarity_search_with_ranking(self, query: str, document_limit: int) -> Dict[str, int]:
 
+        score_uid_info = self.similarity_search_with_score(query, document_limit)
+
+        scores = [] # [<score_doc_1>, <score_doc_2>]
+
+        for score in score_uid_info.values():
+            scores.append(score)
+
+        ranking = self.rank_keywords(scores)
+
+        for index, key in enumerate(score_uid_info):
+            score_uid_info[key] = ranking[index]
+        
+        return score_uid_info
+        
+    
+    def similarity_search_with_score(self, query: str, document_limit: int) -> Dict[str, float]:
+
+        sql = """
+            WITH semantic_search AS (
+                SELECT uuid, document, (1 - (embedding <#> %s::vector)) AS similarity
+                FROM langchain_pg_embedding
+                ORDER BY similarity DESC
+                LIMIT %s
+            )
+            SELECT * FROM semantic_search;
         """
-            If tables doesn't exists
-            Initializes tables langchain_pg_collection and langchain_pg_embeddings with appropriate datatypes
-        """
 
-        check_table_pg_collection = f"""
-            SELECT EXISTS (
-                SELECT 1 FROM pg_tables 
-                WHERE schemaname = 'public' AND tablename = 'langchain_pg_collection'
-            );
-        """
-        self.cursor.execute(check_table_pg_collection)
-        exists = self.cursor.fetchone()[0]
+        single_vector = self.embeddings.embed_query(query)
+        self.cursor.execute(sql, (single_vector,document_limit))
 
-        logging.info(f"table langchain_pg_collection exists = {exists}")
+        results = self.cursor.fetchall()
 
-        if not exists:
-            
-            create_pg_collection = """
-                CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-                CREATE TABLE langchain_pg_collection (
-                    uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-                    name VARCHAR(255) NOT NULL,
-                    cmetadata JSON
-                );
-            """
+        score_uid_info = {} # {'<uuid>': <score>}
 
-            self.cursor.execute(create_pg_collection)
-            logging.info("table langchain_pg_collection created")
+        for row in results:
+            score_uid_info[row[0]] = row[-1]
+        
+        return score_uid_info
+    
+
+    def kw_search(self, query: str, document_limit: int) -> List[Document]:
+        
+        doc_ranking = self.kw_search_with_ranking(query, document_limit)
+        docs = doc_ranking.keys()
+        return self._fetch_documents(list(docs))
         
 
+    def kw_search_with_ranking(self, query: str, document_limit: int) -> Dict[str, int]:
 
-        check_table_pg_embedding = f"""
-            SELECT EXISTS (
-                SELECT 1 FROM pg_tables 
-                WHERE schemaname = 'public' AND tablename = 'langchain_pg_embedding'
-            );
+        score_uid_info = self.kw_search_with_score(query, document_limit)
+
+        scores = [] # [<score_doc_1>, <score_doc_2>]
+
+        for score in score_uid_info.values():
+            scores.append(score)
+
+        ranking = self.rank_keywords(scores)
+
+        for index, key in enumerate(score_uid_info):
+            score_uid_info[key] = ranking[index]
+        
+        return score_uid_info
+
+    def kw_search_with_score(self, query: str, document_limit: int) -> Dict[str, float]:
+
+        sql = """
+            SELECT uuid, document, ts_rank_cd(search_vector, query) AS rank
+            FROM langchain_pg_embedding, plainto_tsquery(%s) query
+            WHERE search_vector @@ query
+            ORDER BY rank DESC
+            LIMIT %s;
+        """
+
+        self.cursor.execute(sql, (query,document_limit))
+
+        results = self.cursor.fetchall()
+
+        score_uid_info = {} # {'<uuid>': <score>}
+
+        for row in results:
+            score_uid_info[row[0]] = row[-1]
+        
+        return score_uid_info
+
+
+    
+
+    def reciprocal_rank_fusions(self, rank1: List[Dict[str,int]], rank2: List[Dict[str,int]], filter_docs: Optional[int]) -> List[str]:
+
+        """
+            Reco[rpcal rank fusion combines and rerank documents obtained from different searching method
+            uses formula:
+                score(d) = SUMMATION ( 1 / ( k + r(d) ) )
+            where:
+                K = parameter constant, originally k=60 used in research paper
+                r(d) = rank of the document d
+
+            combining 2 document ranking:
+                rrf_score =  1 / (k + rank1) + 1 / (k + rank2) # for document["<document_uid>"] of rank1 and rank2
+        """
+
+        if filter_docs > max(len(rank1), len(rank2)):
+            filter_docs = None
+
+        combined_score_uid_info = {}
+        max_document_rank1 = len(rank1) + 1
+        max_document_rank2 = len(rank2) + 1
+
+
+        for key in rank1:
+            combined_score_uid_info[key] = self._reciprocal_rank_fusion_formula(rank1[key], rank2.get(key, max_document_rank1)) # use max rank as len of rank1 list to remove unfair advantage in long lists
+
+        for key in rank2:
+            combined_score_uid_info[key] = self._reciprocal_rank_fusion_formula(rank2[key], rank1.get(key, max_document_rank2)) # use max rank as len of rank2 list to remove unfair advantage in long lists
+
+        for key in combined_score_uid_info:
+            print(key, combined_score_uid_info[key])
+
+        sorted_combined_rank = [k for k, v in sorted(combined_score_uid_info.items(), key=lambda item: item[1], reverse=True)] # Get sorted keys according to
+        
+        if filter_docs == None:
+            return sorted_combined_rank
+        else:
+            return sorted_combined_rank[:filter_docs]
+
+    def add_documents(self, documents: List[Document]):
+
+        sql = """
+            INSERT INTO langchain_pg_embedding (
+                collection_id, embedding, document, cmetadata, custom_id
+            ) VALUES %s;
         """
 
 
-        self.cursor.execute(check_table_pg_embedding)
-        exists = self.cursor.fetchone()[0]
+        text_docs = [doc.page_content for doc in documents]
+        embeddings = self.embeddings.embed_documents(text_docs)
 
-        logging.info(f"table langchain_pg_embedding exists = {exists}")
+        insert_data = []
 
-        if not exists:
-            
-            create_pg_collection = f"""
-                CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+        for doc, embedding in zip(documents, embeddings):
+            insert_data.append((self.collection_uuid, embedding, doc.page_content, json.dumps(doc.metadata), str(uuid4())))
 
-                CREATE TABLE langchain_pg_embedding (
-                    collection_id UUID NOT NULL,
-                    embedding VECTOR(%s),
-                    document VARCHAR(1000),
-                    cmetadata JSON,
-                    custom_id VARCHAR,
-                    uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4()
-                );
+        psycopg2.extras.execute_values(self.cursor, sql, insert_data, template="(%s, %s, %s, %s, %s)")
+        logging.info("Document Inserted!")
 
-                ALTER TABLE langchain_pg_embedding ADD COLUMN search_vector tsvector GENERATED ALWAYS AS (to_tsvector('english', document)) STORED;
-            """
 
-            self.cursor.execute(create_pg_collection, (self.embedding_dimension,))
-            logging.info("table langchain_pg_embedding created")
+    @staticmethod
+    def rank_keywords(kw_score: List[int]) -> List[int]:
+        
+        """
+        Ranks keywords based on their scores in descending order.
+
+        Args:
+            kw_score (list): List of keyword scores.
+
+        Returns:
+            list: Ranked indices starting from 1.
+        """
+
+        sorted_indices = sorted(range(len(kw_score)), key=lambda i: kw_score[i], reverse=True)
+        ranking = [0] * len(kw_score)
+        
+        for rank, idx in enumerate(sorted_indices, start=1):
+            ranking[idx] = rank
+
+        return ranking
     
+
+    def _init_tables(self):
+        """
+        If tables don't exist, initializes `langchain_pg_collection` 
+        and `langchain_pg_embedding` with appropriate datatypes.
+        """
+
+        check_table_pg_collection_sql = """
+            SELECT 
+                EXISTS (SELECT 1 FROM pg_tables WHERE schemaname = 'public' AND tablename = 'langchain_pg_collection'),
+                EXISTS (SELECT 1 FROM pg_tables WHERE schemaname = 'public' AND tablename = 'langchain_pg_embedding');
+        """
+        self.cursor.execute(check_table_pg_collection_sql)
+        check_table_pg_collection_exists, check_table_pg_embedding_exists = self.cursor.fetchone()
+
+        logging.info(f"table langchain_pg_collection exists = {check_table_pg_collection_exists}")
+        logging.info(f"table langchain_pg_embedding exists = {check_table_pg_embedding_exists}")
+
+        if check_table_pg_collection_exists and check_table_pg_embedding_exists:
+            return  # Tables already exist, no need to create them
+
+        create_tables_sql = f"""
+            BEGIN;
+            CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+            -- Create langchain_pg_collection if it does not exist
+            CREATE TABLE IF NOT EXISTS langchain_pg_collection (
+                uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                name VARCHAR(255) NOT NULL,
+                cmetadata JSON
+            );
+
+            -- Create langchain_pg_embedding if it does not exist
+            CREATE TABLE IF NOT EXISTS langchain_pg_embedding (
+                collection_id UUID NOT NULL,
+                embedding VECTOR({self.embedding_dimension}),
+                document VARCHAR(1000),
+                cmetadata JSON,
+                custom_id VARCHAR,
+                uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4()
+            );
+
+            -- Add search_vector column if it does not exist
+            DO $$ 
+            BEGIN
+                IF NOT EXISTS (
+                    SELECT 1 FROM information_schema.columns 
+                    WHERE table_name = 'langchain_pg_embedding' AND column_name = 'search_vector'
+                ) THEN
+                    ALTER TABLE langchain_pg_embedding 
+                    ADD COLUMN search_vector tsvector GENERATED ALWAYS AS (to_tsvector('english', document)) STORED;
+                END IF;
+            END $$;
+
+            COMMIT;
+        """
+
+        self.cursor.execute(create_tables_sql)
+        logging.info("Tables created or already exist")
+
+            
+
     def _create_collection(self):
 
         sql = """
@@ -149,29 +323,30 @@ class HybridPGVector:
         logging.info(f"Collection {self.collection_name} created")
 
 
-    def add_documents(self, documents: List[Document]):
+    def _fetch_documents(self, document_uids: List[str]) -> List[Document]:
 
-        sql = """
-            INSERT INTO langchain_pg_embedding (
-                collection_id, embedding, document, cmetadata, custom_id
-            ) VALUES %s;
-        """
+        sql = "SELECT cmetadata,document FROM langchain_pg_embedding WHERE uuid = ANY(%s::uuid[]);"
+        self.cursor.execute(sql, (document_uids,))
+
+        text_documents = self.cursor.fetchall()        
+        documents = []
+
+        for t_doc in text_documents:
+            documents.append(
+                Document(
+                    metadata = t_doc[0],
+                    page_content = t_doc[1]
+                )
+            )
+
+        return documents
+
+    def _reciprocal_rank_fusion_formula(self, rank1: int, rank2: int) -> int:
+        return 1 / (self.rrf_constant + rank1) + 1 / (self.rrf_constant + rank2)
 
 
-        text_docs = [doc.page_content for doc in documents]
-        embeddings = self.embeddings.embed_documents(text_docs)
 
-        insert_data = []
-
-        for doc, embedding in zip(documents, embeddings):
-            insert_data.append((self.collection_uuid, embedding, doc.page_content, json.dumps(doc.metadata), str(uuid4())))
-
-        psycopg2.extras.execute_values(self.cursor, sql, insert_data, template="(%s, %s, %s, %s, %s)")
-        logging.info("Document Inserted!")
-
-
-
-if __name__ == "__main__":
+if __name__ == "__main__":  
 
     from dotenv import load_dotenv
     from langchain_openai.embeddings import OpenAIEmbeddings
@@ -213,6 +388,13 @@ if __name__ == "__main__":
     hv = HybridPGVector(connection, embeddings_model, collection_name, {})
 
 
+    results = hv.kw_search_with_score("What is Decentralized AI ?", 4)
 
-    hv.add_documents(documents)
+    # a = {'7f739b6a-0614-4f12-a7d3-646d0b00648d': 1}
+
+    # results = hv._fetch_documents(list(a.keys()))
+    print(results)
+    # hv.add_documents(documents)
+
+
 
